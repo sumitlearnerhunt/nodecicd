98b540bca616d7db911782f47fe5b5a3a3679c8c
Sumit Rawat <sumitr@learnerhunt.com>
2024-06-28 12:44:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app=express();
+
+app.get('/api/get',(req,res)=>{
+    res.send({message:"Node js AWS Deployment"})
+})
+
+app.listen(9000,(req,res)=>console.log("Server running at 9000 port"))
