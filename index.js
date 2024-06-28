35e375d7b7c195cec8cb9d9b501512a150636fb5
Sumit Rawat <sumitr@learnerhunt.com>
2024-06-28 17:01:19 +0530

    third commit

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/api/get',(req,res)=>{
     res.send({message:"Node js AWS Deployment 2222"})
 })
 
+app.get('/api/get2',(req,res)=>{
+    res.send({message:"Inky Pinky Ponky"})
+})
+
 app.listen(9000,(req,res)=>console.log("Server running at 9000 port"))
