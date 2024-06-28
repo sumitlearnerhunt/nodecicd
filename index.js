3f05fa0fda25a8a37d1020ea881bf12f90138331
Sumit Rawat <sumitr@learnerhunt.com>
2024-06-28 16:45:21 +0530

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app=express();
 
 app.get('/api/get',(req,res)=>{
-    res.send({message:"Node js AWS Deployment"})
+    res.send({message:"Node js AWS Deployment 2222"})
 })
 
 app.listen(9000,(req,res)=>console.log("Server running at 9000 port"))
